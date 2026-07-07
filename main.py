43a45174d04733aaa0e8be2a7843ba75452d9e5d
hno255 <khdyjhshyl4@gmail.com>
2026-07-07 09:28:33 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print('البوت يعمل!')
+
+bot.run('ضعي_التوكين_هنا')
