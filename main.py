5942dd5739ecea74d10599b51b4f1e28547e3868
hno255 <khdyjhshyl4@gmail.com>
2026-07-07 23:51:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import asyncio
 import random
-
+import os
 bot = commands.Bot(command_prefix='-', intents=discord.Intents.all())
 
 # ضعي رابط صورتك هنا
